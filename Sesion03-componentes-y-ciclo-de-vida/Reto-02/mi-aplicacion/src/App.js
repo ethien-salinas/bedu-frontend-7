5d2f927d6eb9012520aca2b227b2abd5ceb78b75
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2019-11-27 16:33:32 -0600

    [add] solucion a retos

--- Sesion03-componentes-y-ciclo-de-vida/Reto-02/mi-aplicacion/src/App.js
+++ Sesion03-componentes-y-ciclo-de-vida/Reto-02/mi-aplicacion/src/App.js
@@ -1,11 +1,23 @@
+// Como fue mencionado en el archivo index.js, esta línea siempre es necesaria
+// cuando un archivo contiene código de React
 import React from 'react';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-class App extends React.Component {
-  render() {
-    return (
-      <p>Hola Mundo</p>
-    )
-  }
+import "./index.css";
+
+// Este es un componente de React que regresa un `div` con una cadena de texto
+function App() {
+  return (
+    <>
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
+// Tenemos que exportar el componente para poder ser usado en cualquier otro
+// archivo de React
 export default App;
